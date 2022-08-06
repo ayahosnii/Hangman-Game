9028fdfe66755fdab4d90b53cd9c38821278df5a
Aya Hosny <ayazaine@gmail.com>
2022-08-06 23:58:52 +0200

    Random Property And Value

--- js/main.js
+++ js/main.js
@@ -1,18 +1,64 @@
+// Letters
 const letters = "abcdefghijklmnopqrstuvwxyz";
-let lettersArray =Array.from(letters)
 
-let lettersContainer = document.querySelector('.letters')
+// Get Array From Letters
+let lettersArray = Array.from(letters);
 
-lettersArray.forEach(letter=>{
+// Select Letters Container
+let lettersContainer = document.querySelector(".letters");
+
+// Generate Letters
+lettersArray.forEach(letter => {
+
   // Create Span
-  let span = document.createElement("span")
+  let span = document.createElement("span");
 
-  let theLetter = document.createTextNode(letter)
+  // Create Letter Text Node
+  let theLetter = document.createTextNode(letter);
 
-  span.appendChild(theLetter)
+  // Append The Letter To Span
+  span.appendChild(theLetter);
 
-  span.className = 'letter-box'
+  // Add Class On Span
+  span.className = 'letter-box';
 
+  // Append Span To The Letters Container
   lettersContainer.appendChild(span);
-})
 
+});
+
+const words = {
+  programming: ["php", "javascript", "go", "scala", "fortran", "r", "mysql", "python"],
+  movies: ["Prestige", "Inception", "Parasite", "Interstellar", "Whiplash", "Memento", "Coco", "Up"],
+  people: ["Albert Einstein", "Hitchcock", "Alexander", "Cleopatra", "Mahatma Ghandi"],
+  countries: ["Syria", "Palestine", "Yemen", "Egypt", "Bahrain", "Qatar"]
+}
+
+//Get Random Property
+
+let allKeys = Object.keys(words)
+/**1- Get Random Key Number like [0,1,2,3]
+ Depend On Key
+ **/
+let randomPropNumber = Math.floor(Math.random() * allKeys.length) //=> the number of the category
+console.log(randomPropNumber)
+  /**1- Get Random Key Name like  [programming, movies, people, countries]
+   Depend On Key
+   **/
+
+let randomPropName = allKeys[randomPropNumber] //=> the name of the category
+console.log(randomPropName)
+
+//2- Get The Array Of The Key Name Of The Array
+let randomPropValue = words[randomPropName] //=> the array of category
+
+
+//3- Get Random Key Value Of That Random Array [1,2,3..etc]
+let randomValueNumber = Math.floor(Math.random() * randomPropValue.length)
+
+
+//4- Get The Value Of That Key
+let randomValueValue = randomPropValue[randomValueNumber]
+
+//Set Category Info
+document.querySelector('.game-info .category span').innerHTML = randomPropName
