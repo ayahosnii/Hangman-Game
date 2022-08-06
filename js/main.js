bae0670a32309641ee21fb99c7a59f4dfd4699c9
Aya Hosny <ayazaine@gmail.com>
2022-08-07 00:12:50 +0200

    Random Property And Value

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ console.log(randomPropName)
 let randomPropValue = words[randomPropName] //=> the array of category
 
 
-//3- Get Random Key Value Of That Random Array [1,2,3..etc]
+//3- Get Random Value Of That Random Array [1,2,3..etc]
 let randomValueNumber = Math.floor(Math.random() * randomPropValue.length)
 
 
